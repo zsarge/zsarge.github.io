2cb4108aa91f8951fe905003c26c399c68d68ad1
zsarge <zackarywsargent@gmail.com>
2021-03-06 18:20:09 -0500

    add deploy script

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import 'bootstrap-css-only/css/bootstrap.min.css'
-import 'mdbvue/lib/css/mdb.min.css'
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbvue/lib/css/mdb.min.css";
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from "~/layouts/Default.vue";
 
-export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+export default function(Vue, { router, head, isClient }) {
+	// Set default layout as a global component
+	Vue.component("Layout", DefaultLayout);
 }
