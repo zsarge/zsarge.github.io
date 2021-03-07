74e58e6f14b8169ba761e0d5fe4dddf84a5a2ab7
zsarge <zackarywsargent@gmail.com>
2021-03-06 22:22:54 -0500

    automatically generate blogroll

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,10 +9,20 @@
     <div slot="content">
       <g-image alt="Example image" src="~/favicon.png" width="135" />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-        excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-        Dolores, aperiam non officia eos quod asperiores
+        Lorem ipsumasdasd dolor sit amet, consectetur adipisicing elit. Pariatur
+        excepturi labore teasdasdasdmpore expedita, et iste tenetur suscipit
+        explicabo! Dolores, aperiam non officia eos quod asperiores
       </p>
+
+      <h2>Automatic Blogroll</h2>
+
+      <ul>
+        <li v-for="post in $page.posts.edges" :key="post.id">
+          <g-link :to="post.node.path">
+            {{ post.node.title }}
+          </g-link>
+        </li>
+      </ul>
 
       <p class="home-links">
         <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
@@ -28,6 +38,22 @@
     </div>
   </Layout>
 </template>
+
+
+<page-query>
+query Posts {
+    posts: allPost {
+      edges {
+        node {
+          id
+          title
+          path
+        }
+      }
+    }
+  }
+
+</page-query>
 
 <script>
 export default {
