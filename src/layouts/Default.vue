ff1d39b1362c862ea9cdeed337a5a8ba8f599501
zsarge <zackarywsargent@gmail.com>
2021-04-17 13:44:37 -0400

    remove outdated code

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="layout">
     <NavBar />
-    <!-- <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header> -->
     <Page>
       <div slot="header">
         <slot name="header" />
