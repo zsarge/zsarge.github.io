29f1431b0e75039baec1893da037edce76fe4347
Zack Sargent <zackarywsargent@gmail.com>
2023-03-02 20:25:12 -0500

    parse files

--- blog.rb
+++ blog.rb
@@ -4,8 +4,11 @@
 # in that the goal is to create a blog, while
 # minimizing the amount of code needed to maintain it.
 
+require 'date'
+
 SOURCE_PATH = File.join(__dir__, 'content')
 OUTPUT_PATH = File.join(__dir__, 'build')
+ARTICLES_PATH = File.join(SOURCE_PATH, 'articles')
 
 def set_up_files
   def touch file_name
@@ -16,17 +19,55 @@ def set_up_files
   raise "directories exist" if dirs.any? { Dir.exist? _1 }
   dirs.each { Dir.mkdir _1 }
 
-  root_files = %w(index.html header.html footer.html)
-  # create empty files
-  root_files.each { touch _1 }
+  %w(header.html footer.html).each { touch _1 }
 
-  Dir.mkdir(File.join(SOURCE_PATH, "articles"))
+  Dir.mkdir(ARTICLES_PATH)
 
-  touch "/articles/DELME.md"
+  touch File.join(ARTICLES_PATH, 'article_1.md')
+end
+
+class Article 
+  attr_reader :title, :url, :filename, :date, :tags, :author, :content
+  def initialize(filepath:)
+    @author = 'Zack Sargent'
+
+    @filename = File.basename(filepath, ".md")
+    @url = "/articles/#{@filename}.html"
+  
+    File.open(filepath) do |file|
+      # title
+      if file.readline =~ /^Title: (.*)$/
+        @title = $1.strip
+      else
+        raise "Invalid title in '#{filename}'"
+      end
+
+      # date
+      if file.readline =~ /^Date: (\d{4}\-\d{1,2}\-\d{1,2})$/
+        @date = Date.parse($1)
+      else
+        raise "Invalid date in '#{filename}'"
+      end
+
+      # tags
+      if file.readline =~ /^Tags: (.*)$/
+        @tags = $1.gsub(/[[:space:]]/, '').split(',')
+      else
+        raise "Invalid tags in '#{filename}'"
+      end
+
+      raise "Line 4 must be blank in #{filename}" unless file.readline.strip == ''
+      raise "Line 5 must be blank in #{filename}" unless file.readline.strip == ''
+
+      @content = file.read # get rest of file
+    end
+  end
 end
 
 def generate_blog
-  puts "GENERATING"
+  Dir["#{ARTICLES_PATH}/*"].each do |file|
+    p Article.new(filepath: file)
+  end
 end
 
 
