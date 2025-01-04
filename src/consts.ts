91235ee62b84b49710afb9e144466a5dbd31c8e0
Zack Sargent <zack@zack.fyi>
2025-01-04 16:02:23 -0500

    update contact info

--- src/consts.ts
+++ src/consts.ts
@@ -5,5 +5,6 @@ export const TWITTER_HANDLE = "https://twitter.com/abdllah_dev";
 export const GITHUB_HANDLE = "https://github.com/zsarge";
 export const FEDIVERSE_URL = "https://defcon.social/@zsarge";
 export const BLUESKY_URL = "https://bsky.app/profile/zack.fyi";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/zsarge";
 
 export const CLOUDINARY_NAME = "example";
